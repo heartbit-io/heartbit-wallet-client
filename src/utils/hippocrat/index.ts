e38e9ba5cf58fd8600df2183750d3faca4465f2b
Hyunhum <hyeonheum@naver.com>
2023-02-03 17:11:40 +0900

    feat: add LNPayment module in hippocrat

--- src/utils/hippocrat/index.ts
+++ src/utils/hippocrat/index.ts
@@ -2,6 +2,7 @@ import BtcPayment from "./src/BtcPayment";
 import BtcWallet from "./src/BtcWallet";
 import BtcRpcNode from "./src/BtcRpcNode";
 import RareData from "./src/RareData";
+import LightningPayment from "./src/LightningPayment";
 import BtcNetwork from "./src/enums/BtcNetwork";
 import BtcRpcUrl from "./src/enums/BtcRpcUrl";
 import TxFee from "./src/enums/TxFee";
@@ -9,7 +10,8 @@ import BtcAccount from "./src/models/BtcAccount";
 import BtcReceiver from "./src/models/BtcReceiver";
 import BtcSigner from "./src/models/BtcSigner";
 import UTXO from "./src/models/UTXO";
+import BOLT11 from "./src/models/BOLT11";
 
-export { BtcPayment, BtcWallet, BtcRpcNode, RareData };
+export { BtcPayment, BtcWallet, BtcRpcNode, RareData, LightningPayment };
 export { BtcNetwork, BtcRpcUrl, TxFee };
-export type { BtcAccount, BtcReceiver, BtcSigner, UTXO};
+export { BtcAccount, BtcReceiver, BtcSigner, UTXO, BOLT11 };
