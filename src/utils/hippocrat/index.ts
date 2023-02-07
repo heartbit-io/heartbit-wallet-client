f32804dac7ce0c33177cbb6e6727a2da62989e0d
Hyunhum <hyeonheum@naver.com>
2023-02-07 14:38:28 +0900

    refactor: rename LN module in an abbreviated form

--- src/utils/hippocrat/index.ts
+++ src/utils/hippocrat/index.ts
@@ -2,7 +2,7 @@ import BtcPayment from "./src/BtcPayment";
 import BtcWallet from "./src/BtcWallet";
 import BtcRpcNode from "./src/BtcRpcNode";
 import RareData from "./src/RareData";
-import LightningPayment from "./src/LightningPayment";
+import LNPayment from "./src/LNPayment";
 import BtcNetwork from "./src/enums/BtcNetwork";
 import BtcRpcUrl from "./src/enums/BtcRpcUrl";
 import TxFee from "./src/enums/TxFee";
@@ -12,6 +12,6 @@ import BtcSigner from "./src/models/BtcSigner";
 import UTXO from "./src/models/UTXO";
 import BOLT11 from "./src/models/BOLT11";
 
-export { BtcPayment, BtcWallet, BtcRpcNode, RareData, LightningPayment };
+export { BtcPayment, BtcWallet, BtcRpcNode, RareData, LNPayment };
 export { BtcNetwork, BtcRpcUrl, TxFee };
 export { BtcAccount, BtcReceiver, BtcSigner, UTXO, BOLT11 };
