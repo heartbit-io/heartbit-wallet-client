fb64826d5848aef85e7d831642ee595a4a77afb8
Hyunhum <hyeonheum@naver.com>
2023-02-09 18:56:45 +0900

    feat: add login tab

--- src/navigators/TabNavigator.tsx
+++ src/navigators/TabNavigator.tsx
@@ -6,6 +6,7 @@ import Settings from '../screens/settings/Settings';
 import Welcome from '../screens/wallet/Welcome';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import SignUp from '../screens/wallet/SignUp';
+import LogIn from '../screens/wallet/LogIn';
 const Tab = createBottomTabNavigator();
 
 function TabNavigator(): JSX.Element {
@@ -16,14 +17,19 @@ function TabNavigator(): JSX.Element {
           headerShown: false,
         })}>
         <Tab.Screen
+          name="Welcome"
+          component={Welcome}
+          options={{title: 'Welcome'}}
+        />
+        <Tab.Screen
           name="SignUp"
           component={SignUp}
           options={{title: 'Sign Up'}}
         />
         <Tab.Screen
-          name="Welcome"
-          component={Welcome}
-          options={{title: 'Welcome'}}
+          name="LogIn"
+          component={LogIn}
+          options={{title: 'Log In'}}
         />
         <Tab.Screen name="Home" component={Home} options={{title: 'Home'}} />
         <Tab.Screen
