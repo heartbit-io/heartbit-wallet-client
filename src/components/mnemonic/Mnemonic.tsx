7a32230e4129465690267ea62edb57ff35ba7e94
Hyunhum <hyeonheum@naver.com>
2023-02-10 15:01:41 +0900

    feat: improve UI for welcome mnemonic page

--- src/components/mnemonic/Mnemonic.tsx
+++ src/components/mnemonic/Mnemonic.tsx
@@ -1,4 +1,4 @@
-import {Button, Text, TouchableOpacity} from 'react-native';
+import {Button, Text, TouchableOpacity, StyleSheet} from 'react-native';
 import React, {useState} from 'react';
 import {
   generateMnemonic, selectMnemonic
@@ -18,23 +18,44 @@ export function Mnemonic() {
 
   return (
     <>
+    <Text style={styles.text}>Generate your own recovery pharse</Text>
+    <Text></Text>  
     <Text>
       <Button onPress={() => dispatch(generateMnemonic())} 
-      title="generate mnemonic" />
+      title="generate recovery phrase" />
     </Text>
     <Text></Text>
-    <Text>
+    <Text style={styles.bigText}>
         {mnemonic.split(" ").slice(0, 6).map(word => word + " ")}
     </Text>
-    <Text>
+    <Text style={styles.bigText}>
         {mnemonic.split(" ").slice(6, 12).map(word => word + " ")}
     </Text>
     <Text></Text>
     <TouchableOpacity onPress={() => copyToClipboard()}>
-      <Text>
+      <Text style={styles.smallText}>
         Copy To Clipboard
       </Text>
     </TouchableOpacity>
+    <Text></Text>
+    <Text style={styles.text}>Please save your recovery phrase in very safe place</Text>
+    <Text style={styles.text}>If revealed, your fund and data will be stolen</Text>
     </>
   );
 }
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 14,
+    fontWeight: 'bold'
+  },
+  smallText: {
+    fontSize: 10,
+    fontWeight: 'bold'
+  },
+  bigText: {
+    fontSize: 18,
+    fontWeight: 'bold'
+  }
+});
+
