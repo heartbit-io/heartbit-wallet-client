4c588a7a39dd02a243fa2395d27924c201d650b2
Hyunhum <hyeonheum@naver.com>
2023-02-13 16:41:40 +0900

    feat: nullify storage clear as it's just for test

--- src/RootApp.tsx
+++ src/RootApp.tsx
@@ -1,8 +1,6 @@
 import React, {useEffect, useState} from 'react';
-import TabNavigator from './navigators/TabNavigator';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import WalletNavigator from './navigators/WalletNavigator';
-import LogIn from './screens/wallet/LogIn';
 import LogInNavigator from './navigators/LogInNavigator';
 
 function RootApp(): JSX.Element {
@@ -11,7 +9,7 @@ function RootApp(): JSX.Element {
 
   useEffect(() => {
     const checkMnemonic = async () => {
-      await AsyncStorage.clear();
+      //await AsyncStorage.clear();
       const mnemonicState = await AsyncStorage.getItem("vault");
       mnemonicState === null ?
       setIsUser(false) : setIsUser(true);
