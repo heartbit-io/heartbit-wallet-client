04bce196ab79f5bd5cab3179ca8db31695529a8a
Hyunhum <hyeonheum@naver.com>
2023-02-13 15:45:40 +0900

    feat: Auth rendering implemented

--- src/RootApp.tsx
+++ src/RootApp.tsx
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import TabNavigator from './navigators/TabNavigator';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import WalletNavigator from './navigators/WalletNavigator';
+import LogIn from './screens/wallet/LogIn';
+import LogInNavigator from './navigators/LogInNavigator';
 
 function RootApp(): JSX.Element {
-  return <TabNavigator />;
+
+  const [isUser, setIsUser] = useState(false);
+
+  useEffect(() => {
+    const checkMnemonic = async () => {
+      await AsyncStorage.clear();
+      const mnemonicState = await AsyncStorage.getItem("vault");
+      mnemonicState === null ?
+      setIsUser(false) : setIsUser(true);
+    }
+    checkMnemonic();
+  })
+
+  return isUser? <LogInNavigator /> : <WalletNavigator />
+
 }
 
 export default RootApp;
