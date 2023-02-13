f985c7af6db29881a1cd6405cd663afa1db45b01
Hyunhum <hyeonheum@naver.com>
2023-02-13 15:42:29 +0900

    feat: get rid of container for TabNavigator

--- src/navigators/TabNavigator.tsx
+++ src/navigators/TabNavigator.tsx
@@ -1,49 +1,28 @@
 import Home from '../screens/home/Home';
 import MyDataStackNavigator from './MyDataStackNavigator';
-import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
 import Settings from '../screens/settings/Settings';
-import Welcome from '../screens/wallet/Welcome';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import SignUp from '../screens/wallet/SignUp';
-import LogIn from '../screens/wallet/LogIn';
 const Tab = createBottomTabNavigator();
 
 function TabNavigator(): JSX.Element {
   return (
-    <NavigationContainer>
-      <Tab.Navigator
-        screenOptions={() => ({
-          headerShown: false,
-        })}>
-        <Tab.Screen
-          name="Welcome"
-          component={Welcome}
-          options={{title: 'Welcome'}}
-        />
-        <Tab.Screen
-          name="SignUp"
-          component={SignUp}
-          options={{title: 'Sign Up'}}
-        />
-        <Tab.Screen
-          name="LogIn"
-          component={LogIn}
-          options={{title: 'Log In'}}
-        />
-        <Tab.Screen name="Home" component={Home} options={{title: 'Home'}} />
-        <Tab.Screen
-          name="MyDataStackNavigator"
-          component={MyDataStackNavigator}
-          options={{title: 'My Data', tabBarBadge: 1}}
-        />
-        <Tab.Screen
-          name="Settings"
-          component={Settings}
-          options={{title: 'Settings'}}
-        />
-      </Tab.Navigator>
-    </NavigationContainer>
+    <Tab.Navigator
+      screenOptions={() => ({
+        headerShown: false,
+      })}>
+      <Tab.Screen name="Home" component={Home} options={{title: 'Home'}} />
+      <Tab.Screen
+        name="MyDataStackNavigator"
+        component={MyDataStackNavigator}
+        options={{title: 'My Data', tabBarBadge: 1}}
+      />
+      <Tab.Screen
+        name="Settings"
+        component={Settings}
+        options={{title: 'Settings'}}
+      />
+    </Tab.Navigator>
   );
 }
 
