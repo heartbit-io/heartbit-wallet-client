7248dfc56f40da482c24b0536f376a25fd569b33
Hyunhum <hyeonheum@naver.com>
2023-02-14 16:32:07 +0900

    feat: add BTC balance & receive component in home

--- src/screens/home/Home.tsx
+++ src/screens/home/Home.tsx
@@ -1,13 +1,18 @@
 import {ScrollView, Text, View} from 'react-native';
 
 import React from 'react';
+import { Receive } from '../../components/bitcoin/Receive';
+import { Balance } from '../../components/bitcoin/Balance';
 
 function Home() {
   return (
     <ScrollView>
       <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-        <Text>**********Redux Tookit Test Counter**********</Text>
-        <Text>**********Redux Tookit Test Counter**********</Text>
+        <Text>HeartBit</Text>
+        <Text></Text>
+        <Balance />
+        <Text></Text>
+        <Receive />
       </View>
     </ScrollView>
   );
