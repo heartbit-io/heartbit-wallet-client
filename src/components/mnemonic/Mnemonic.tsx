8b0b065d7e0e6a4f40c69a8f7f506dac30133732
Hyunhum <hyeonheum@naver.com>
2023-03-07 12:57:08 +0900

    feat: better UX for creating wallet

--- src/components/mnemonic/Mnemonic.tsx
+++ src/components/mnemonic/Mnemonic.tsx
@@ -7,7 +7,7 @@ import {useAppDispatch, useAppSelector} from '../../hooks/hooks';
 import Clipboard from '@react-native-clipboard/clipboard';
 import { ThunkDispatch } from '@reduxjs/toolkit';
 
-export function Mnemonic() {
+export function Mnemonic({navigation}: {navigation: any}) {
   const dispatch : ThunkDispatch = useAppDispatch();
 
   const mnemonic : string = useAppSelector(selectMnemonic);
@@ -17,42 +17,75 @@ export function Mnemonic() {
   };
 
   return (
-    <>
-    <Text style={styles.text}>Welcome to HeartBit Wallet!</Text>
-    <Text style={styles.text}>To use our service,</Text>
-    <Text style={styles.text}>Please generate your own recovery phrase!</Text>
-    <Text></Text>  
-    <Text>
-      <Button onPress={() => dispatch(generateMnemonic())} 
-      title="generate recovery phrase" />
-    </Text>
-    <Text></Text>
-    <View style={styles.mnemonic}>
-    <Text style={styles.text}>
-        {mnemonic.length === 0 ? "" :
-        mnemonic.split(" ").slice(0, 4).map((word, index) => index+1 + ". " + word + "  ")}
-    </Text>
-    <Text style={styles.text}>
-        {mnemonic.split(" ").slice(4, 8).map((word, index) => index+5 + ". " + word + "  ")}
-    </Text>
-    <Text style={styles.text}>
-        {mnemonic.split(" ").slice(8, 12).map((word, index) => index+9 + ". " + word + "  ")}
-    </Text>
-    </View>
-    <Text></Text>
-    <TouchableOpacity onPress={() => copyToClipboard()}>
-      <Text style={styles.smallText}>
-        Copy To Clipboard
+    <View style={styles.columnContainer}>
+      <Text style={styles.text}>Welcome to HeartBit Wallet👋</Text>
+      <Text style={styles.text}>To use our service,</Text>
+      <Text style={styles.text}>Please create your own wallet key🙏</Text>
+      <Text></Text>  
+      <TouchableOpacity style={styles.button} onPress={() => dispatch(generateMnemonic())}>
+        <Text style={styles.bigText}>Create Wallet Key</Text>
+      </TouchableOpacity>
+      <Text></Text>
+      <View style={styles.mnemonic}>
+      <Text style={styles.text}>
+          {mnemonic.length === 0 ? "" :
+          mnemonic.split(" ").slice(0, 4).map((word, index) => index+1 + ". " + word + "  ")}
       </Text>
-    </TouchableOpacity>
-    <Text></Text>
-    <Text style={styles.text}>Please save your recovery phrase in very safe place</Text>
-    <Text style={styles.text}>If revealed, your fund and data will be stolen</Text>
-    </>
+      <Text style={styles.text}>
+          {mnemonic.split(" ").slice(4, 8).map((word, index) => index+5 + ". " + word + "  ")}
+      </Text>
+      <Text style={styles.text}>
+          {mnemonic.split(" ").slice(8, 12).map((word, index) => index+9 + ". " + word + "  ")}
+      </Text>
+      </View>
+      <Text></Text>
+      <TouchableOpacity onPress={() => copyToClipboard()}>
+        <Text style={styles.smallText}>
+          Copy To Clipboard
+        </Text>
+      </TouchableOpacity>
+      <Text></Text>
+      <Text style={styles.text}>Please save your wallet key somewhere safe for back up,</Text>
+      <Text style={styles.text}>In case to recover in other device💻</Text>
+      <Text style={styles.text}>Be aware! If revealed, your fund and data could be stolen😱</Text>
+      <Text></Text>
+      <TouchableOpacity style={styles.button} onPress={() => navigation.replace('SignUp')}>
+         <Text style={styles.bigText}>Set Password to Login</Text>
+      </TouchableOpacity>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  columnContainer: {
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  rowContainer: {
+    flex: 0.3,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+  },
+  contentArea: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  button: {
+    width: '80%',
+    height: '10%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderColor: 'black',
+    borderWidth: 2,
+    borderRadius: 10
+  },
   text: {
     fontSize: 14,
     fontWeight: 'bold',
@@ -71,9 +104,9 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     borderWidth: 0.5,
     borderColor: 'black',
+    backgroundColor: 'white',
     alignItems: 'center',
     textAlign: 'center',
     justifyContent: 'center',
   }
-});
-
+})
--- src/screens/auth/Welcome.tsx
+++ src/screens/auth/Welcome.tsx
@@ -1,36 +1,20 @@
-import {ScrollView, Button, Text, View, StyleSheet} from 'react-native';
+import {SafeAreaView, TouchableOpacity, Text, View, StyleSheet} from 'react-native';
 
 import {Mnemonic} from '../../components/mnemonic/Mnemonic';
-import {MnemonicRestore} from '../../components/mnemonic/MnemonicRestore';
 import React from 'react';
 
 function Welcome({navigation}: {navigation: any}) {
   return (
-    <View style={styles.container}>
-      <Mnemonic />
-      <Text></Text>
-      <Button onPress={() => navigation.replace('SignUp')} 
-      title="set password to log in" />
-      <Text></Text>
-      <Text>OR</Text>
-      <Text></Text>
-      <MnemonicRestore />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Mnemonic navigation = {navigation} />
+    </SafeAreaView>
   );
 }
+
+export default Welcome;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    margin: 15,
-    borderRadius: 25,
-    alignItems: 'center',
-    textAlign: 'center',
-    justifyContent: 'center',
-    backgroundColor: 'white'
   }
-});
-
-
-
-export default Welcome;
+})
