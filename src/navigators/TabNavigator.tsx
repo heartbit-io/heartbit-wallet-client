26219d1c6308a1b9797baa44c5634577b7788c40
ChrisCho-H <hyeonheum@naver.com>
2023-04-05 18:23:01 +0900

    style: apply lint for pull request #3

--- src/navigators/TabNavigator.tsx
+++ src/navigators/TabNavigator.tsx
@@ -17,12 +17,18 @@ function TabNavigator(): JSX.Element {
 				tabBarIcon: ({ focused, color, size }) => {
 					let iconName;
 					if (route.name === 'HomeNavigator') {
-						iconName = focused ? require('../assets/img/ic_home_selected.png') : require('../assets/img/ic_home_unselected.png');
+						iconName = focused
+							? require('../assets/img/ic_home_selected.png')
+							: require('../assets/img/ic_home_unselected.png');
 					} else if (route.name === 'WalletNavigator') {
-						iconName = focused ? require('../assets/img/ic_wallet_selected.png') : require('../assets/img/ic_wallet_unselected.png');
+						iconName = focused
+							? require('../assets/img/ic_wallet_selected.png')
+							: require('../assets/img/ic_wallet_unselected.png');
 					}
 
-					return <Image source={iconName} style={{ width: size, height: size }} />;
+					return (
+						<Image source={iconName} style={{ width: size, height: size }} />
+					);
 				},
 			})}
 		>
