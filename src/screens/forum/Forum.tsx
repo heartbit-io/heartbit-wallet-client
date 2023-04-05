a07beacc6e895c841bcb8d7769725afc9088e9d3
devmasme <devmasme@gmail.com>
2023-04-05 19:55:26 +0900

    fix: unimplemented screens annotation

--- src/screens/forum/Forum.tsx
+++ src/screens/forum/Forum.tsx
@@ -1,24 +1,26 @@
 import { ScrollView, StyleSheet, View } from 'react-native';
-import React from 'react';
+
 import AskContent from '../../components/home/AskContent';
-import GPT4 from '../../components/forum/GPT4';
-import Question from '../../components/forum/Question';
-import Doctor from '../../components/forum/Doctor';
-import BestAnswer from '../../components/forum/BestAnswer';
+import React from 'react';
+
+// import GPT4 from '../../components/forum/GPT4';
+// import Question from '../../components/forum/Question';
+// import Doctor from '../../components/forum/Doctor';
+// import BestAnswer from '../../components/forum/BestAnswer';
 
 function Forum({ navigation }: { navigation: any }) {
 	return (
 		<ScrollView style={styles.container}>
 			<View style={styles.columnContainerTop}>
-				<Question />
+				{/* <Question /> */}
 			</View>
 			<View style={styles.columnContainer}>
-				<GPT4 />
+				{/* <GPT4 /> */}
 			</View>
 			<View style={styles.columnContainer}>
-				<Doctor />
+				{/* <Doctor /> */}
 			</View>
-			<BestAnswer navigation={navigation} />
+			{/* <BestAnswer navigation={navigation} /> */}
 		</ScrollView>
 	);
 }
