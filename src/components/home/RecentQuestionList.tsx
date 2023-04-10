092f5a308fbb5d7ea1a5e1c3acddd9d988b9e64f
ChrisCho-H <hyeonheum@naver.com>
2023-04-10 14:56:59 +0900

    refactor: seperate questionList component

---
+++ src/components/home/RecentQuestionList.tsx
@@ -0,0 +1,133 @@
+import { View, StyleSheet, Text } from 'react-native';
+
+import React from 'react';
+
+function RecentQuestionList() {
+	return (
+		<>
+			<View style={styles.recentHeaderContainer}>
+				<Text style={styles.recentHeaderLeftText}>Recent</Text>
+				<Text style={styles.recentHeaderRightText}>See all {'>'}</Text>
+			</View>
+			{/* FIXME(david): Just For demo */}
+			<View style={styles.recentQuestion}>
+				<Text style={styles.recentQuestionTopText}>Mild migraine</Text>
+				<Text style={styles.recentHeaderMiddleText}>
+					2 mins ago, No answer 1,000 sats
+				</Text>
+				<Text style={styles.recentHeaderBottomText_blue}>
+					Collecting answers: 2 days 23 hours left
+				</Text>
+				<View style={styles.line} />
+			</View>
+			<View style={styles.recentQuestion}>
+				<Text style={styles.recentQuestionTopText}>
+					Cataracts and MTX steroids
+				</Text>
+				<Text style={styles.recentHeaderMiddleText}>
+					13 Mar 2023, 2 answers 10,000 sats
+				</Text>
+				<Text style={styles.recentHeaderBottomText_red}>
+					Select an answer within 1 day 1 hour {'>'}{' '}
+				</Text>
+				<View style={styles.line} />
+			</View>
+			<View style={styles.recentQuestion}>
+				<Text style={styles.recentQuestionTopText}>Stomachache</Text>
+				<Text style={styles.recentHeaderMiddleText}>
+					2 mins ago, No answer 1,000 sats
+				</Text>
+				<Text style={styles.recentHeaderBottomText_blue}>
+					Collecting answers: 2 days 23 hours left
+				</Text>
+				<View style={styles.line} />
+			</View>
+		</>
+	);
+}
+
+export default RecentQuestionList;
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: '#FFF5ED',
+	},
+	columnContainer: {
+		flex: 1,
+		flexDirection: 'column',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	recentQuestionContainer: {
+		flex: 1,
+	},
+	recentHeaderContainer: {
+		flexDirection: 'row',
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+	recentHeaderLeftText: {
+		fontSize: 28,
+		fontWeight: 'bold',
+		marginLeft: 16,
+		color: '#8E8E93',
+		marginBottom: 10,
+		marginRight: 'auto',
+	},
+	recentHeaderRightText: {
+		fontSize: 17,
+		marginRight: 16,
+		color: '#8E8E93',
+		marginBottom: 10,
+		marginLeft: 'auto',
+	},
+	recentQuestion: {
+		marginHorizontal: 16,
+		marginVertical: 5,
+	},
+	line: {
+		borderBottomWidth: 1,
+		borderBottomColor: '#ddd',
+		paddingTop: 13,
+	},
+	recentQuestionTopText: {
+		fontSize: 20,
+		fontWeight: 'bold',
+	},
+	recentHeaderMiddleText: {
+		fontSize: 15,
+		color: '#8E8E93',
+		marginTop: 5,
+		marginBottom: 5,
+	},
+	recentHeaderBottomText_blue: {
+		fontSize: 15,
+		color: '#007AFF',
+	},
+	recentHeaderBottomText_red: {
+		fontSize: 15,
+		color: '#FF2D55',
+	},
+	rowContainer: {
+		flex: 0.3,
+		flexDirection: 'row',
+		alignItems: 'center',
+		justifyContent: 'space-around',
+	},
+	contentArea: {
+		flex: 1,
+		paddingTop: '20%',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	gradient: {
+		width: '100%',
+		height: '50%',
+	},
+	text: {
+		fontSize: 16,
+		fontWeight: 'bold',
+		marginHorizontal: 8,
+	},
+});
--- src/screens/home/Home.tsx
+++ src/screens/home/Home.tsx
@@ -4,6 +4,7 @@ import { ScrollView, StatusBar, StyleSheet, Text, View } from 'react-native';
 import AskButton from '../../components/home/AskButton';
 import Intro from '../../components/home/Intro';
 import LinearGradient from 'react-native-linear-gradient';
+import RecentQuestionList from '../../components/home/RecentQuestionList';
 
 function Home({ navigation }: { navigation: any }) {
 	useEffect(() => {
@@ -19,43 +20,7 @@ function Home({ navigation }: { navigation: any }) {
 					<AskButton navigation={navigation} />
 				</View>
 				<View style={styles.recentQuestionContainer}>
-					<View style={styles.recentHeaderContainer}>
-						<Text style={styles.recentHeaderLeftText}>Recent</Text>
-						<Text style={styles.recentHeaderRightText}>See all {'>'}</Text>
-					</View>
-					{/* FIXME(david): Just For demo */}
-					<View style={styles.recentQuestion}>
-						<Text style={styles.recentQuestionTopText}>Mild migraine</Text>
-						<Text style={styles.recentHeaderMiddleText}>
-							2 mins ago, No answer 1,000 sats
-						</Text>
-						<Text style={styles.recentHeaderBottomText_blue}>
-							Collecting answers: 2 days 23 hours left
-						</Text>
-						<View style={styles.line} />
-					</View>
-					<View style={styles.recentQuestion}>
-						<Text style={styles.recentQuestionTopText}>
-							Cataracts and MTX steroids
-						</Text>
-						<Text style={styles.recentHeaderMiddleText}>
-							13 Mar 2023, 2 answers 10,000 sats
-						</Text>
-						<Text style={styles.recentHeaderBottomText_red}>
-							Select an answer within 1 day 1 hour {'>'}{' '}
-						</Text>
-						<View style={styles.line} />
-					</View>
-					<View style={styles.recentQuestion}>
-						<Text style={styles.recentQuestionTopText}>Stomachache</Text>
-						<Text style={styles.recentHeaderMiddleText}>
-							2 mins ago, No answer 1,000 sats
-						</Text>
-						<Text style={styles.recentHeaderBottomText_blue}>
-							Collecting answers: 2 days 23 hours left
-						</Text>
-						<View style={styles.line} />
-					</View>
+					<RecentQuestionList />
 				</View>
 			</LinearGradient>
 		</ScrollView>
@@ -77,54 +42,7 @@ const styles = StyleSheet.create({
 	},
 	recentQuestionContainer: {
 		flex: 1,
-	},
-	recentHeaderContainer: {
-		flexDirection: 'row',
-		justifyContent: 'center',
-		alignItems: 'center',
-	},
-	recentHeaderLeftText: {
-		fontSize: 28,
-		fontWeight: 'bold',
-		marginLeft: 16,
-		color: '#8E8E93',
-		marginBottom: 10,
-		marginRight: 'auto',
-	},
-	recentHeaderRightText: {
-		fontSize: 17,
-		marginRight: 16,
-		color: '#8E8E93',
-		marginBottom: 10,
-		marginLeft: 'auto',
-	},
-	recentQuestion: {
-		marginLeft: 16,
-		marginTop: 5,
-		marginBottom: 5,
-	},
-	line: {
-		borderBottomWidth: 1,
-		borderBottomColor: '#ddd',
-		paddingTop: 13,
-	},
-	recentQuestionTopText: {
-		fontSize: 20,
-		fontWeight: 'bold',
-	},
-	recentHeaderMiddleText: {
-		fontSize: 15,
-		color: '#8E8E93',
-		marginTop: 5,
-		marginBottom: 5,
-	},
-	recentHeaderBottomText_blue: {
-		fontSize: 15,
-		color: '#007AFF',
-	},
-	recentHeaderBottomText_red: {
-		fontSize: 15,
-		color: '#FF2D55',
+		flexDirection: 'column',
 	},
 	rowContainer: {
 		flex: 0.3,
