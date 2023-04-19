561ae2754742435ada0ee12648f848ddb1d5b1f3
nick-heartbit <nick@heartbit.io>
2023-04-19 11:14:50 +0500

    refactor store folder

--- src/store/reducers.ts
+++ src/store/reducers.ts
@@ -1,12 +1,8 @@
 import { combineReducers } from '@reduxjs/toolkit';
 
 // slices
-import MnemonicReducer from '../components/mnemonic/MnemonicSlice';
-import BtcAddressReducer from '../components/bitcoin/BtcAddressSlice';
-import DIDReducer from '../components/did/DIDSlice';
+import DIDReducer from 'components/did/DIDSlice';
 
 export default combineReducers({
-	mnemonic: MnemonicReducer,
-	btcAddress: BtcAddressReducer,
 	DID: DIDReducer,
 });
