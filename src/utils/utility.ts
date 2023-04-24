2f61388621c6d5625715700286d3576751f90816
nick-heartbit <nick@heartbit.io>
2023-04-24 08:18:40 +0500

    add KeyboardAwareScrollView and fix keyboard hiding input issue in Email sign up screen

---
+++ src/utils/utility.ts
@@ -0,0 +1,5 @@
+import { Dimensions, Platform } from 'react-native';
+
+export const OS = Platform.OS;
+export const dw = Dimensions.get('window').width;
+export const dh = Dimensions.get('window').height;
