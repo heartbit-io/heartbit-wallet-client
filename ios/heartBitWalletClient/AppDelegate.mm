7f20c6ed507555ac07f5e9ba428353a1298c829f
nick-heartbit <nick@heartbit.io>
2023-04-24 11:31:11 +0500

    set up firebase fore ios

--- ios/heartBitWalletClient/AppDelegate.mm
+++ ios/heartBitWalletClient/AppDelegate.mm
@@ -1,4 +1,5 @@
 #import "AppDelegate.h"
+#import <Firebase.h>
 
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
@@ -11,6 +12,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [FIRApp configure];
   self.moduleName = @"heartBitWalletClient";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
