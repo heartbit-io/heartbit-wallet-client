91e6ddcabfcbceb63b417ae011be3e86a25ef390
nick-heartbit <nick@heartbit.io>
2023-04-24 10:28:54 +0500

    remove Dimenstions.get() in utility

--- src/utils/utility.ts
+++ src/utils/utility.ts
@@ -1,5 +1,3 @@
-import { Dimensions, Platform } from 'react-native';
+import { Platform } from 'react-native';
 
 export const OS = Platform.OS;
-export const dw = Dimensions.get('window').width;
-export const dh = Dimensions.get('window').height;
