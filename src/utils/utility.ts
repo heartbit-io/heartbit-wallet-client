d97c9b9f0f6f9e9f9dee21dda039403577cc3ec8
nick-heartbit <nick@heartbit.io>
2023-04-26 19:12:07 +0500

    validate email logic added and disable MainButton when active is false

--- src/utils/utility.ts
+++ src/utils/utility.ts
@@ -1,3 +1,11 @@
 import { Platform } from 'react-native';
 
 export const OS = Platform.OS;
+
+export const validateEmail = (email: string) => {
+	return email
+		.toLowerCase()
+		.match(
+			/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+		);
+};
