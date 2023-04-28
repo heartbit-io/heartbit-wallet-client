d8151694afd9ac74e9cf8674eaef6225f07bde6a
nick-heartbit <nick@heartbit.io>
2023-04-28 15:18:54 +0500

    fix ios firebase authentication error dynamic linking issue

--- src/hooks/useFirebaseLink.ts
+++ src/hooks/useFirebaseLink.ts
@@ -1,23 +1,23 @@
 import { useCallback, useEffect, useState } from 'react';
+import { Linking } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import dynamicLinks, {
-	FirebaseDynamicLinksTypes,
-} from '@react-native-firebase/dynamic-links';
+import dynamicLinks from '@react-native-firebase/dynamic-links';
 
 // hooks
 import useSignIn from './useSignIn';
 
-const useFirebaseLink = (): [boolean, boolean] => {
-	const [onSignIn, isLoading, isError] = useSignIn();
+// utils
+import { OS } from 'utils/utility';
+
+const useFirebaseLink = (): [boolean, boolean, boolean] => {
+	const [onSignIn, isSignedIn, isLoading, isError] = useSignIn();
 	const [isSignInError, setSignInError] = useState(false);
 
-	const handleDynamicLink = useCallback<
-		(link: FirebaseDynamicLinksTypes.DynamicLink) => void
-	>(
-		async link => {
+	const handleDynamicLink = useCallback<(url: string) => void>(
+		async url => {
 			try {
 				const email = await AsyncStorage.getItem('email');
-				onSignIn(link.url, email);
+				onSignIn(url, email);
 			} catch (error) {
 				console.error('Error while signing user in:', error);
 				setSignInError(true);
@@ -27,36 +27,52 @@ const useFirebaseLink = (): [boolean, boolean] => {
 	);
 
 	useEffect(() => {
-		const unsubscribe = dynamicLinks().onLink(handleDynamicLink);
-		return () => {
-			unsubscribe();
-		};
+		if (OS === 'android') {
+			const unsubscribe = dynamicLinks().onLink(link =>
+				handleDynamicLink(link.url),
+			);
+			return () => {
+				unsubscribe();
+			};
+		}
 	}, [handleDynamicLink]);
 
 	useEffect(() => {
-		let unsubscribed = false;
+		if (OS === 'android') {
+			let unsubscribed = false;
 
-		async function getInitialLink() {
-			const initialLink = await dynamicLinks().getInitialLink();
+			async function getInitialLink() {
+				const initialLink = await dynamicLinks().getInitialLink();
 
-			if (initialLink && !unsubscribed) {
-				handleDynamicLink(initialLink);
+				if (initialLink && !unsubscribed) {
+					handleDynamicLink(initialLink.url);
+				}
 			}
+			getInitialLink();
+
+			return () => {
+				unsubscribed = true;
+			};
+
+			// Remove handleDynamicLink from the deps array to avoid redundant calls.
+
+			// This hook should be called only once, at start.
+
+			// eslint-disable-next-line react-hooks/exhaustive-deps
 		}
-		getInitialLink();
-
-		return () => {
-			unsubscribed = true;
-		};
-
-		// Remove handleDynamicLink from the deps array to avoid redundant calls.
-
-		// This hook should be called only once, at start.
-
-		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, []);
 
-	return [isLoading, isError || isSignInError];
+	useEffect(() => {
+		if (OS === 'ios') {
+			const linking = Linking.addEventListener('url', ({ url }) => {
+				handleDynamicLink(url);
+			});
+
+			return () => linking.remove();
+		}
+	}, []);
+
+	return [isSignedIn, isLoading, isError || isSignInError];
 };
 
 export default useFirebaseLink;
