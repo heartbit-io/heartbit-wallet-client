8ee0fe59b84dbf79b5374d33d76e374bf938094c
ChrisCho-H <hyeonheum@naver.com>
2023-04-28 18:49:43 +0900

    refactor: apply styled component and refactor modal

--- src/screens/home/Bounty.tsx
+++ src/screens/home/Bounty.tsx
@@ -2,8 +2,14 @@ import styled from 'styled-components/native';
 import React, { useState } from 'react';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { MainButton } from 'components';
-import { BountyChoiceList } from './BountyChoiceList';
-import { LargeTitle } from 'components/common';
+import { BountyInfoModal } from './BountyInfoModal';
+import {
+	LargeTitle,
+	Body,
+	Caption1,
+	Footnote,
+	Subheadline,
+} from 'components/common';
 import { postQuestion } from 'apis/postQuestion';
 import { Alert } from 'react-native';
 
@@ -33,9 +39,53 @@ function Bounty({ navigation, route }: Props) {
 	};
 
 	return (
-		<Wrapper>
-			<Text>Set a bounty for human answers</Text>
-			<BountyChoiceList bounty={bounty} setBounty={setBounty} />
+		<ScrollWrapper>
+			<Text>Set a bounty for consultation</Text>
+			<BountyInfoModal />
+			<Text></Text>
+			<BountyButton10000
+				bounty={bounty}
+				onPress={() => {
+					setBounty(10000);
+				}}
+			>
+				<BountyTextBold>
+					High: <BountyText>Likely get answered faster</BountyText>
+				</BountyTextBold>
+				<BountyAmountContainer>
+					<BountySatoshiText>10,000 sats</BountySatoshiText>
+					<BountyUSDText>2.85 USD</BountyUSDText>
+				</BountyAmountContainer>
+			</BountyButton10000>
+			<BountyButton1000
+				bounty={bounty}
+				onPress={() => {
+					setBounty(1000);
+				}}
+			>
+				<BountyTextBold>Medium</BountyTextBold>
+				<BountyAmountContainer>
+					<BountySatoshiText>1,000 sats</BountySatoshiText>
+					<BountyUSDText>0.28 USD</BountyUSDText>
+				</BountyAmountContainer>
+			</BountyButton1000>
+			<BountyButton100
+				bounty={bounty}
+				onPress={() => {
+					setBounty(100);
+				}}
+			>
+				<BountyTextBold>
+					Low: <BountyText>Likely get answered slower</BountyText>
+				</BountyTextBold>
+				<BountyAmountContainer>
+					<BountySatoshiText>100 sats</BountySatoshiText>
+					<BountyUSDText>0.03 USD</BountyUSDText>
+				</BountyAmountContainer>
+			</BountyButton100>
+			<BalanceWrapper>
+				<BalanceText>Balance: 2,393,042 sats</BalanceText>
+			</BalanceWrapper>
 			<MainButton
 				onPress={async () =>
 					await navigateToForum({ navigation, route }, bounty)
@@ -44,7 +94,7 @@ function Bounty({ navigation, route }: Props) {
 				buttonStyle={{ marginBottom: 37 }}
 				active={bounty === 0 ? false : true}
 			/>
-		</Wrapper>
+		</ScrollWrapper>
 	);
 }
 
@@ -54,11 +104,91 @@ const Text = styled(LargeTitle)`
 	font-weight: bold;
 `;
 
-const Wrapper = styled.View`
+const ScrollWrapper = styled.ScrollView`
 	flex: 1;
 	flex-direction: column;
-	align-items: flex-start;
-	justify-content: flex-start;
-	padding-horizontal: 25px;
 	background-color: #fff5ed;
+	padding-horizontal: 25px;
+`;
+
+const BountyText = styled(Footnote)`
+	font-weight: normal;
+`;
+
+const BountyTextBold = styled(Footnote)`
+	font-weight: bold;
+	margin-left: 16px;
+`;
+
+const BountySatoshiText = styled(Body)`
+	font-weight: bold;
+	margin-top: 11px;
+	margin-bottom: 4px;
+`;
+
+const BountyUSDText = styled(Caption1)`
+	color: #8e8e93;
+	margin-bottom: 11px;
+`;
+
+const BountyAmountContainer = styled.View`
+	flex: 1;
+	flex-direction: column;
+	align-items: flex-end;
+	justify-content: flex-end;
+	margin-right: 16px;
+`;
+
+const BountyButton10000 = styled.TouchableOpacity<{ bounty?: number }>`
+	flex: 1;
+	flex-direction: row;
+	width: 100%;
+	height: 64px;
+	align-items: center;
+	justify-content: center;
+	border-color: ${({ bounty }) => (bounty === 10000 ? 'red' : 'gray')};
+	background-color: white;
+	border-width: ${({ bounty }) => (bounty === 10000 ? '2px' : '1px')};
+	border-radius: 12px;
+	margin-bottom: 8px;
+`;
+
+const BountyButton1000 = styled.TouchableOpacity<{ bounty?: number }>`
+	flex: 1;
+	flex-direction: row;
+	width: 100%;
+	height: 64px;
+	align-items: center;
+	justify-content: center;
+	border-color: ${({ bounty }) => (bounty === 1000 ? 'red' : 'gray')};
+	background-color: white;
+	border-width: ${({ bounty }) => (bounty === 1000 ? '2px' : '1px')};
+	border-radius: 12px;
+	margin-bottom: 8px;
+`;
+
+const BountyButton100 = styled.TouchableOpacity<{ bounty?: number }>`
+	flex: 1;
+	flex-direction: row;
+	width: 100%;
+	height: 64px;
+	align-items: center;
+	justify-content: center;
+	border-color: ${({ bounty }) => (bounty === 100 ? 'red' : 'gray')};
+	background-color: white;
+	border-width: ${({ bounty }) => (bounty === 100 ? '2px' : '1px')};
+	border-radius: 12px;
+`;
+
+const BalanceText = styled(Subheadline)`
+	color: #8e8e93;
+`;
+
+const BalanceWrapper = styled.View`
+	flex: 1;
+	justify-content: center;
+	align-items: center;
+	width: 100%;
+	margin-top: 144px;
+	margin-bottom: 32px;
 `;
