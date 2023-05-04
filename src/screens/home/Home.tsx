0e4cb6c6842b5bf65b75bda078666f6bfbe9ef13
ChrisCho-H <hyeonheum@naver.com>
2023-05-04 19:27:11 +0900

    feat: apply styled component and fetch recentQuenstions

--- src/screens/home/Home.tsx
+++ src/screens/home/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { ScrollView, StatusBar, StyleSheet, Text, View } from 'react-native';
 import RecentQuestionList from './RecentQuestionList';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
@@ -7,11 +7,50 @@ import styled from 'styled-components/native';
 import logo from 'assets/logo/logo.svg';
 
 // components
-import { Gradient, MainButton } from 'components';
+import {
+	Body,
+	Gradient,
+	Headline,
+	MainButton,
+	Subheadline,
+	Title1,
+} from 'components';
+import { getQuestionList } from 'apis/questionApi';
 
 type Props = NativeStackScreenProps<HomeNavigatorParamList, 'Home'>;
 
 function Home({ navigation }: Props) {
+	const [questions, setQuestions] = useState<GetQuestionResponse[]>([
+		{
+			id: 0,
+			userId: 0,
+			content: '',
+			bountyAmount: 0,
+			status: '',
+			updatedAt: '',
+			createdAt: '',
+		},
+	]);
+	const getDateFormatted = (createdAt?: string) => {
+		const dateList: string[] =
+			createdAt === undefined || createdAt === ''
+				? new Date().toDateString().split(' ')
+				: new Date(createdAt as string).toDateString().split(' ');
+		return `${dateList[2]} ${dateList[1]} ${dateList[3]}` as string;
+	};
+
+	useEffect(() => {
+		(async () => {
+			try {
+				const responseDto: ResponseDto<GetQuestionResponse[]> =
+					await getQuestionList(10, 0);
+				setQuestions(responseDto.data as GetQuestionResponse[]);
+			} catch (err) {
+				console.error(err);
+			}
+		})();
+	}, []);
+
 	return (
 		<Gradient>
 			<ScrollView>
@@ -24,7 +63,29 @@ function Home({ navigation }: Props) {
 					/>
 				</Wrapper>
 				<WrapperNotCenter>
-					<RecentQuestionList />
+					<RecentWrapper>
+						<HeaderWrapper>
+							<TextTitle>Recent</TextTitle>
+							<TextBody>See all {'>'}</TextBody>
+						</HeaderWrapper>
+						{questions[0].status === '' ? (
+							<></>
+						) : (
+							questions.map(question => {
+								return (
+									<QuestionWrapper>
+										<TextHeadline>
+											{question.content.slice(0, 30).concat('...')}
+										</TextHeadline>
+										<TextSubHeadline>
+											{getDateFormatted(question.createdAt)} ・{' '}
+											{question.bountyAmount.toLocaleString()} sats
+										</TextSubHeadline>
+									</QuestionWrapper>
+								);
+							})
+						)}
+					</RecentWrapper>
 				</WrapperNotCenter>
 			</ScrollView>
 		</Gradient>
@@ -45,3 +106,37 @@ const WrapperNotCenter = styled.View`
 `;
 
 const Logo = styled.Image``;
+
+const RecentWrapper = styled.View`
+	flex-direction: column;
+	margin-horizontal: 16px;
+`;
+const HeaderWrapper = styled.View`
+	flex-direction: row;
+	justify-content: space-between;
+	align-items: center;
+	margin-bottom: 16px;
+`;
+const QuestionWrapper = styled.View`
+	flex-direction: column;
+	justify-content: center;
+	align-items: flex-start;
+	width: 100%;
+	height: 82px;
+	border-bottom-width: 0.5px;
+	border-color: #bdbdbd;
+`;
+const TextBody = styled(Body)`
+	color: gray;
+`;
+const TextTitle = styled(Title1)`
+	font-weight: bold;
+	color: gray;
+`;
+const TextHeadline = styled(Headline)`
+	font-weight: bold;
+	margin-bottom: 8px;
+`;
+const TextSubHeadline = styled(Subheadline)`
+	color: gray;
+`;
