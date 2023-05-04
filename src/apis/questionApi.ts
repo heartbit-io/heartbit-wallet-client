eaad1c80a48fcefe2cffa8c96fe2d331839e2104
ChrisCho-H <hyeonheum@naver.com>
2023-05-04 13:51:52 +0900

    feat: add postGPTReply and getReply api

--- src/apis/questionApi.ts
+++ src/apis/questionApi.ts
@@ -1,12 +1,12 @@
 import { api } from 'apis';
 
+const url = 'questions';
+
 export const postQuestion = async (
 	askContent: string,
 	bounty: number,
 ): Promise<ResponseDto<CreateQuestionResponse>> => {
 	try {
-		const url = 'questions';
-
 		const response = await api.post(url, {
 			content: askContent,
 			bountyAmount: bounty,
@@ -15,6 +15,40 @@ export const postQuestion = async (
 		const responseDto = await response.data;
 
 		return responseDto as ResponseDto<CreateQuestionResponse>;
+	} catch (err: any) {
+		return err;
+	}
+};
+
+export const postGPTReply = async (
+	questionId: number,
+): Promise<ResponseDto<ReplyResponse>> => {
+	try {
+		const gptPath = '/chat-gpt-replies';
+
+		const response = await api.post(url + gptPath, {
+			questionId,
+		});
+
+		const responseDto = await response.data;
+
+		return responseDto as ResponseDto<ReplyResponse>;
+	} catch (err: any) {
+		return err;
+	}
+};
+
+export const getReply = async (
+	questionId: number,
+): Promise<ResponseDto<ReplyResponse>> => {
+	try {
+		const replyPath = `/${questionId}/replies`;
+
+		const response = await api.get(url + replyPath);
+
+		const responseDto = await response.data;
+
+		return responseDto as ResponseDto<ReplyResponse>;
 	} catch (err: any) {
 		return err;
 	}
