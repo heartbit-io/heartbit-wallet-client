ac1668e18099c84b483467ce5292fd90ac2232cd
ChrisCho-H <hyeonheum@naver.com>
2023-05-08 13:39:27 +0900

    feat: add Got answered component upon status

--- src/screens/home/Home.tsx
+++ src/screens/home/Home.tsx
@@ -74,20 +74,25 @@ function Home({ navigation }: Props) {
 						</EmptyWrapper>
 					) : (
 						<RecentWrapper>
-							<HeaderWrapper>
+							<SpaceWrapper>
 								<TextTitle>Recent</TextTitle>
 								<TextBody>See all {'>'}</TextBody>
-							</HeaderWrapper>
+							</SpaceWrapper>
 							{questions.map(question => {
 								return (
 									<QuestionWrapper>
 										<TextHeadline>
 											{question.content.slice(0, 30).concat('...')}
 										</TextHeadline>
-										<TextSubHeadlineGray>
-											{getDateFormatted(question.createdAt)} ・{' '}
-											{question.bountyAmount.toLocaleString()} sats
-										</TextSubHeadlineGray>
+										<SpaceWrapper>
+											<TextSubHeadlineGray>
+												{getDateFormatted(question.createdAt)} ・{' '}
+												{question.bountyAmount.toLocaleString()} sats
+											</TextSubHeadlineGray>
+											<TextSubHeadlineBlue>
+												{question.status === 'Open' ? '' : 'Got answered ✓'}
+											</TextSubHeadlineBlue>
+										</SpaceWrapper>
 									</QuestionWrapper>
 								);
 							})}
@@ -126,16 +131,14 @@ const RecentWrapper = styled.View`
 	flex-direction: column;
 	margin-horizontal: 16px;
 `;
-const HeaderWrapper = styled.View`
+const SpaceWrapper = styled.View`
 	flex-direction: row;
 	justify-content: space-between;
 	align-items: center;
-	margin-bottom: 16px;
 `;
 const QuestionWrapper = styled.View`
 	flex-direction: column;
 	justify-content: center;
-	align-items: flex-start;
 	width: 100%;
 	height: 82px;
 	border-bottom-width: 0.5px;
@@ -143,10 +146,12 @@ const QuestionWrapper = styled.View`
 `;
 const TextBody = styled(Body)`
 	color: gray;
+	margin-bottom: 16px;
 `;
 const TextTitle = styled(Title1)`
 	font-weight: bold;
 	color: gray;
+	margin-bottom: 16px;
 `;
 const TextHeadline = styled(Headline)`
 	font-weight: bold;
@@ -155,7 +160,9 @@ const TextHeadline = styled(Headline)`
 const TextSubHeadlineGray = styled(Subheadline)`
 	color: gray;
 `;
-
+const TextSubHeadlineBlue = styled(Subheadline)`
+	color: #007aff;
+`;
 const TextSubHeadline = styled(Subheadline)`
 	margin-top: 40px;
 	margin-horizontal: 70px;
