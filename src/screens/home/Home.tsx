0c988c2d5dc745db3acef7e9ff49acc3b4e2ca1f
nick-heartbit <nick@heartbit.io>
2023-05-25 15:47:15 +0500

    refactor and used reusable components in Home screen

--- src/screens/home/Home.tsx
+++ src/screens/home/Home.tsx
@@ -1,129 +1,83 @@
-import React, { useCallback, useEffect, useState } from 'react';
-import { ScrollView, StatusBar, StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from 'react';
+import { ActivityIndicator, ScrollView } from 'react-native';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import styled from 'styled-components/native';
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 // assets
 import logo from 'assets/logo/logo.svg';
-import empty from 'assets/img/ic_empty_recent_q.png';
-import menu from 'assets/img/ic_menu.svg';
-import sat from 'assets/img/ic_sat.png';
+import empty from 'assets/img/emptyArrowQ.svg';
 
 // components
 import {
-	Body,
-	Callout,
+	ArrowButtonWithText,
+	EmptyList,
 	Gradient,
-	Headline,
+	HomeHeader,
 	MainButton,
-	Subheadline,
-	Title1,
+	QuestionListItem,
 } from 'components';
-import { getQuestionList } from 'apis/questionApi';
 
-import moment from 'moment';
+// hooks
 import { useAppDispatch, useAppSelector } from 'hooks/hooks';
-import { DrawerActions, useFocusEffect } from '@react-navigation/native';
-import { getTransactionsList } from 'store/slices/transactionsSlice';
 
-// utils
-import { OS } from 'utils/utility';
+// store
+import { getTransactionsList } from 'store/slices/transactionsSlice';
+import { fetchQuestionsList } from 'store/slices/questionsSlice';
 
 type Props = NativeStackScreenProps<HomeNavigatorParamList, 'Home'>;
 
 function Home({ navigation }: Props) {
-	const statusBarHeight = useSafeAreaInsets().top;
 	const dispatch = useAppDispatch();
-	const { userData } = useAppSelector(state => state.user);
-	const [questions, setQuestions] = useState<GetQuestionResponse[]>([]);
-	const getDateFormatted = (createdAt?: string) => {
-		return createdAt === undefined || createdAt === ''
-			? moment().format('MMM D YYYY')
-			: moment(createdAt).format('MMM D YYYY');
-	};
-
-	useFocusEffect(
-		useCallback(() => {
-			dispatch(getTransactionsList());
-			(async () => {
-				try {
-					const responseDto: ResponseDto<GetQuestionResponse[]> =
-						await getQuestionList(10, 0);
-					setQuestions(responseDto.data as GetQuestionResponse[]);
-				} catch (err) {
-					console.error(err);
-				}
-			})();
-			console.log(userData);
-		}, [questions?.length]),
+	const { questions, questionsLoading } = useAppSelector(
+		state => state.questions,
 	);
+
+	useEffect(() => {
+		dispatch(getTransactionsList());
+		dispatch(fetchQuestionsList());
+	}, []);
 
 	return (
 		<Gradient>
-			<HeaderWrapper statusBarHeight={statusBarHeight}>
-				<Satoshi>
-					<SatoshiIcon source={sat} />
-					<TextCallout> {userData?.btcBalance.toLocaleString()}</TextCallout>
-				</Satoshi>
-				<ButtonWrapper
-					onPress={() => {
-						navigation.dispatch(DrawerActions.openDrawer());
-					}}
-				>
-					<BurgerIcon source={menu} />
-				</ButtonWrapper>
-			</HeaderWrapper>
-			<ScrollView scrollEnabled={questions?.length > 0}>
-				<Wrapper>
-					<Logo source={logo} />
+			<HomeHeader />
+			<Wrapper>
+				<LogoWrapper>
+					<Icon source={logo} />
 					<MainButton
 						text={'Ask doctors anything'}
 						onPress={() => navigation.navigate('Ask')}
-						buttonStyle={{ marginTop: 95 }}
+						buttonStyle={{ marginTop: 85, height: 50 }}
 					/>
-				</Wrapper>
-				<WrapperNotCenter>
-					{questions === undefined || questions?.length === 0 ? (
-						<EmptyWrapper>
-							<Empty source={empty} />
-							<TextSubHeadline>
-								Get your first health consultation from doctors all over the
-								world
-							</TextSubHeadline>
-						</EmptyWrapper>
-					) : (
-						<RecentWrapper>
-							<SpaceWrapper>
-								<TextTitle>Recent</TextTitle>
-								<ButtonWrapper
-									onPress={() => navigation.navigate('MyQuestions')}
-								>
-									<TextBody>See all {'>'}</TextBody>
-								</ButtonWrapper>
-							</SpaceWrapper>
-							{questions?.map(question => {
-								return (
-									<QuestionWrapper>
-										<TextHeadline numberOfLines={1}>
-											{question.content}
-										</TextHeadline>
-										<SpaceWrapper>
-											<TextSubHeadlineGray>
-												{getDateFormatted(question.createdAt)} ・{' '}
-												{question.bountyAmount.toLocaleString()} sats
-											</TextSubHeadlineGray>
-											<TextSubHeadlineBlue>
-												{question.status === 'Open' ? '' : 'Got answered ✓'}
-											</TextSubHeadlineBlue>
-										</SpaceWrapper>
-									</QuestionWrapper>
-								);
-							})}
-						</RecentWrapper>
-					)}
-				</WrapperNotCenter>
-			</ScrollView>
+				</LogoWrapper>
+				{questionsLoading ? (
+					<ActivityIndicator
+						color={'#F68F2A'}
+						size={'large'}
+						style={{ marginTop: 40 }}
+					/>
+				) : questions?.length === 0 ? (
+					<EmptyList
+						icon={empty}
+						text={
+							'Get your first health consultation\nfrom doctors all over the world'
+						}
+						wrapperStyle={{ marginTop: 40 }}
+					/>
+				) : (
+					<RecentWrapper>
+						<ArrowButtonWithText
+							title="Recent"
+							btnText="See all"
+							onPress={() => navigation.navigate('MyQuestions')}
+						/>
+						<ScrollView>
+							{questions?.map(question => (
+								<QuestionListItem key={question.id} question={question} />
+							))}
+						</ScrollView>
+					</RecentWrapper>
+				)}
+			</Wrapper>
 		</Gradient>
 	);
 }
@@ -132,95 +86,19 @@ export default Home;
 
 const Wrapper = styled.View`
 	flex: 1;
+`;
+
+const LogoWrapper = styled.View`
 	align-items: center;
-	padding-top: 121px;
-	padding-horizontal: 25px;
-`;
-const WrapperNotCenter = styled.View`
-	flex: 1;
-	padding-top: 64px;
+	margin-top: 90px;
+	padding-horizontal: 32px;
 `;
 
-const HeaderWrapper = styled.View<{ statusBarHeight: number }>`
-	flex-direction: row;
-	justify-content: space-between;
-	align-items: center;
-	height: ${() => (OS === 'ios' ? 44 : 56)}px;
-	margin-top: ${({ statusBarHeight }) => statusBarHeight}px;
-	margin-horizontal: 16px;
+const Icon = styled.Image`
+	resize-mode: contain;
 `;
-
-const ButtonWrapper = styled.TouchableOpacity``;
-
-const Satoshi = styled.View`
-	flex-direction: row;
-	height: 36px;
-	width: 124px;
-	border-radius: 8px;
-	background: #ffffff66;
-	justify-content: space-between;
-	align-items: center;
-`;
-
-const SatoshiIcon = styled.Image`
-	margin-left: 6px;
-`;
-
-const BurgerIcon = styled.Image``;
-
-const TextCallout = styled(Callout)`
-	margin-right: 12px;
-`;
-
-const Logo = styled.Image``;
-
-const EmptyWrapper = styled.View`
-	flex-direction: column;
-	justify-content: center;
-	align-items: center;
-`;
-
-const Empty = styled.Image``;
 
 const RecentWrapper = styled.View`
-	flex-direction: column;
-	margin-horizontal: 16px;
-`;
-const SpaceWrapper = styled.View`
-	flex-direction: row;
-	justify-content: space-between;
-	align-items: center;
-`;
-const QuestionWrapper = styled.View`
-	flex-direction: column;
-	justify-content: center;
-	width: 100%;
-	height: 82px;
-	border-bottom-width: 0.5px;
-	border-color: #bdbdbd;
-`;
-
-const TextBody = styled(Body)`
-	color: gray;
-	margin-bottom: 16px;
-`;
-const TextTitle = styled(Title1)`
-	font-weight: bold;
-	color: gray;
-	margin-bottom: 16px;
-`;
-const TextHeadline = styled(Headline)`
-	font-weight: bold;
-	margin-bottom: 8px;
-`;
-const TextSubHeadlineGray = styled(Subheadline)`
-	color: gray;
-`;
-const TextSubHeadlineBlue = styled(Subheadline)`
-	color: #007aff;
-`;
-const TextSubHeadline = styled(Subheadline)`
-	margin-top: 40px;
-	margin-horizontal: 70px;
-	margin-bottom: 166px;
+	flex: 1;
+	margin-top: 64px;
 `;
