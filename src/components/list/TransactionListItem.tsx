b28548dae52939749059ce81d6f3526d05302733
nick-heartbit <nick@heartbit.io>
2023-05-26 19:36:11 +0500

    Transaction list item added

---
+++ src/components/list/TransactionListItem.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import moment from 'moment';
+import styled from 'styled-components/native';
+
+// components
+import { Headline, Subheadline } from 'components/common';
+
+type Props = { transaction: TransactionProps };
+
+const TransactionListItem = ({ transaction }: Props) => {
+	return (
+		<ItemWrapper>
+			<Subheadline color="#8E8E93">
+				{moment(transaction.createdAt).format('D MMM YYYY ・ hh:mm')}
+			</Subheadline>
+			<RowWrapper>
+				<HeadlineText>{transaction.type}</HeadlineText>
+				<Subheadline>{`${transaction.amount} sats (${transaction.fee} sats fee)`}</Subheadline>
+			</RowWrapper>
+		</ItemWrapper>
+	);
+};
+
+export default TransactionListItem;
+
+const ItemWrapper = styled.View`
+	border-bottom-width: 0.5px;
+	border-bottom-color: rgba(60, 60, 67, 0.36);
+	padding-vertical: 15px;
+	margin-horizontal: 16px;
+`;
+
+const RowWrapper = styled.View`
+	flex-direction: row;
+	align-items: center;
+	justify-content: space-between;
+	margin-top: 8px;
+`;
+
+const HeadlineText = styled(Headline)`
+	font-weight: bold;
+`;
