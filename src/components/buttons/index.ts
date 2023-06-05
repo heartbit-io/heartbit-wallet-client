d9ebb37d0fd265d056e8e3e4ff4e0c42f84c3e0c
nick-heartbit <nick@heartbit.io>
2023-06-05 12:05:15 +0500

    made reusable BountyButton component

--- src/components/buttons/index.ts
+++ src/components/buttons/index.ts
@@ -2,5 +2,6 @@ import MainButton from './MainButton';
 import Button from './Button';
 import ArrowButtonWithText from './ArrowButtonWithText';
 import SelectButton from './SelectButon';
+import BountyButton from './BountyButton';
 
-export { MainButton, Button, ArrowButtonWithText, SelectButton };
+export { MainButton, Button, ArrowButtonWithText, SelectButton, BountyButton };
