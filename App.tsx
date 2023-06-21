b61be4bba0c8898b82dfd1a0729638185a101e5b
nick-heartbit <nick@heartbit.io>
2023-06-21 14:01:20 +0500

    added intercom launcher in App.tsx

--- App.tsx
+++ App.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StatusBar } from 'react-native';
 import { Provider } from 'react-redux';
 import { ActivityIndicatorProvider } from 'contexts';
+import Intercom from '@intercom/intercom-react-native';
 
 // components
 import Layout from 'routes';
@@ -13,6 +14,10 @@ import { store } from 'store';
 import 'utils/initializeDefaultProps';
 
 function App(): JSX.Element {
+	useEffect(() => {
+		Intercom.setLauncherVisibility('VISIBLE');
+		Intercom.setBottomPadding(50);
+	});
 	return (
 		<Provider store={store}>
 			<ActivityIndicatorProvider>
