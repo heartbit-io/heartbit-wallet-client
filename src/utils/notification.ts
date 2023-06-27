82f08692e37f8b8cf2c2e5ffe8a4e8a09cadb9b3
nick-heartbit <nick@heartbit.io>
2023-06-27 12:44:55 +0500

    update onMessageReceived function

--- src/utils/notification.ts
+++ src/utils/notification.ts
@@ -6,10 +6,19 @@ export const onMessageReceived = (message: any) => {
 		body: message.notification.body,
 		android: {
 			channelId: 'default',
-			smallIcon: 'ic_launcher', // optional, defaults to 'ic_launcher'.
+			smallIcon: 'ic_small_icon', // optional, defaults to 'ic_launcher'.
+			color: '#F68F2A',
 			// pressAction is needed if you want the notification to open the app when pressed
 			pressAction: {
 				id: 'default',
+			},
+		},
+		ios: {
+			foregroundPresentationOptions: {
+				badge: true,
+				sound: true,
+				banner: true,
+				list: true,
 			},
 		},
 	});
