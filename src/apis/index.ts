9c3c5fb555fab35cb7cde05a98ef66013c382c79
nick-heartbit <nick@heartbit.io>
2023-06-28 10:41:11 +0500

    apiKey added in api request header

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,8 +1,11 @@
 import axios from 'axios';
-import { API_URL, LND_API_URL, LND_API_KEY } from '@env';
+import { API_URL, LND_API_URL, LND_API_KEY, API_KEY } from '@env';
 
 export const api = axios.create({
 	baseURL: API_URL,
+	headers: {
+		apiKey: API_KEY,
+	},
 });
 
 export const apiLND = axios.create({
