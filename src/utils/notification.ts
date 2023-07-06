4b9df8d55fc0f5f2a6d3bc908348afec285c6c3f
ChrisCho-H <hyeonheum@naver.com>
2023-07-06 14:29:07 +0900

    fix: create channel for android

--- src/utils/notification.ts
+++ src/utils/notification.ts
@@ -1,25 +1,34 @@
 import notifee from '@notifee/react-native';
 
-export const onMessageReceived = (message: any) => {
-	notifee.displayNotification({
-		title: message.notification.title,
-		body: message.notification.body,
-		android: {
-			channelId: 'default',
-			smallIcon: 'ic_small_icon', // optional, defaults to 'ic_launcher'.
-			color: '#F68F2A',
-			// pressAction is needed if you want the notification to open the app when pressed
-			pressAction: {
-				id: 'default',
+export const onMessageReceived = async (message: any) => {
+	try {
+		const channelId = await notifee.createChannel({
+			id: 'default',
+			name: 'Default Channel',
+		});
+
+		await notifee.displayNotification({
+			title: `<body style="font-size: 16px; font-weight: 500;">${message.notification.title}</body>`,
+			body: message.notification.body,
+			android: {
+				channelId,
+				smallIcon: 'ic_small_icon', // optional, defaults to 'ic_launcher'.
+				color: '#F68F2A',
+				// pressAction is needed if you want the notification to open the app when pressed
+				pressAction: {
+					id: 'default',
+				},
 			},
-		},
-		ios: {
-			foregroundPresentationOptions: {
-				badge: true,
-				sound: true,
-				banner: true,
-				list: true,
+			ios: {
+				foregroundPresentationOptions: {
+					badge: true,
+					sound: true,
+					banner: true,
+					list: true,
+				},
 			},
-		},
-	});
+		});
+	} catch (err) {
+		console.log(err);
+	}
 };
