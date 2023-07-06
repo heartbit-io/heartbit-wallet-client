c3d14552caeae4802ffb332e1aed821d914d5bc1
ChrisCho-H <hyeonheum@naver.com>
2023-07-06 16:31:23 +0900

    feat: add update user balance and tx in callback

--- src/utils/notification.ts
+++ src/utils/notification.ts
@@ -1,4 +1,9 @@
 import notifee from '@notifee/react-native';
+import { getTransactions } from 'apis/transactionsApi';
+import { getUser } from 'apis/userApi';
+import { store } from 'store';
+import { setOffset, setTransactions } from 'store/slices/transactionsSlice';
+import { setUserData, updateUserData } from 'store/slices/userSlice';
 
 export const onMessageReceived = async (message: any) => {
 	try {
@@ -28,6 +33,19 @@ export const onMessageReceived = async (message: any) => {
 				},
 			},
 		});
+		// update user balance and transactions in redux
+		const email = store.getState().user.userData.email;
+		const userResponse = await getUser(email);
+		const txListResponse = await getTransactions(email, 50, 0);
+		userResponse.statusCode === 200
+			? store.dispatch(
+					updateUserData({ btcBalance: userResponse.data.btcBalance }),
+			  )
+			: console.log(userResponse);
+		console.log(txListResponse);
+		txListResponse.statusCode === 200
+			? store.dispatch(setTransactions(txListResponse.data.transactions))
+			: console.log(txListResponse);
 	} catch (err) {
 		console.log(err);
 	}
