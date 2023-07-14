7045a78874d27658cc50213c8739633db8ec6f8d
nick-heartbit <nick@heartbit.io>
2023-07-14 10:37:28 +0500

    coinSlice added in reducers

--- src/store/reducers.ts
+++ src/store/reducers.ts
@@ -5,10 +5,12 @@ import DIDReducer from 'components/did/DIDSlice';
 import userSlice from './slices/userSlice';
 import transactionsSlice from './slices/transactionsSlice';
 import questionsSlice from './slices/questionsSlice';
+import coinSlice from './slices/coinSlice';
 
 export default combineReducers({
 	DID: DIDReducer,
 	user: userSlice,
 	transactions: transactionsSlice,
 	questions: questionsSlice,
+	coin: coinSlice,
 });
