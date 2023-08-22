15e1615668da9282d0b12a12435ba1143902ceaf
nick-heartbit <nick@heartbit.io>
2023-08-23 00:55:29 +0500

    Alpha label added in HeaderTitle

--- src/components/header/HeaderTitle.tsx
+++ src/components/header/HeaderTitle.tsx
@@ -1,38 +1,34 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import styled from 'styled-components/native';
 import { useNavigation } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { DrawerActions } from '@react-navigation/native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 // components
 import { LargeTitle } from 'components/common';
 
 // assets
 import MenuIcon from 'assets/img/ic_menu.svg';
+import Alpha from 'assets/img/alpha.svg';
+
+// utils
 import { OS } from 'utils/utility';
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 const HeaderTitle = () => {
 	const statusBarHeight = useSafeAreaInsets().top;
 
 	const navigation = useNavigation<NativeStackNavigationProp<RootStackType>>();
 
-	const renderHeaderLeft = () => {
-		return <Title>Wallet</Title>;
-	};
-
-	const renderHeaderRight = () => {
-		return (
-			<Wrapper onPress={() => navigation.dispatch(DrawerActions.openDrawer())}>
-				<Icon source={MenuIcon} />
-			</Wrapper>
-		);
-	};
-
 	return (
 		<HeaderWrapper statusBarHeight={statusBarHeight}>
-			{renderHeaderLeft()}
-			{renderHeaderRight()}
+			<Title>Wallet</Title>
+			<RightContainer>
+				<Icon source={Alpha} style={{ marginRight: 16 }} />
+				<Btn onPress={() => navigation.dispatch(DrawerActions.openDrawer())}>
+					<Icon source={MenuIcon} />
+				</Btn>
+			</RightContainer>
 		</HeaderWrapper>
 	);
 };
@@ -54,6 +50,11 @@ const Title = styled(LargeTitle)`
 	color: #1c1c1e;
 `;
 
-const Wrapper = styled.TouchableOpacity``;
+const RightContainer = styled.View`
+	flex-direction: row;
+	align-items: center;
+`;
+
+const Btn = styled.TouchableOpacity``;
 
 const Icon = styled.Image``;
