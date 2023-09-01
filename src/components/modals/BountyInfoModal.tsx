e583548d79c565698ae5644431c69edc861ceb4e
nick-heartbit <nick@heartbit.io>
2023-09-01 13:53:55 +0500

    change Bounty info modal description

--- src/components/modals/BountyInfoModal.tsx
+++ src/components/modals/BountyInfoModal.tsx
@@ -28,10 +28,10 @@ export function BountyInfoModal() {
 					<Container activeOpacity={1}>
 						<Title1 weight="bold">How does the tip work?</Title1>
 						<Body style={{ marginVertical: 32 }}>
-							The bounty is money given to the doctor who answers your question.
-							If you set a higher bounty, you are more likely to get a quick
-							response. This is because questions with higher bounties are given
-							to doctors first.
+							A tip is money given to the doctor who answers your question. If
+							you set a higher tip amount, you are more likely to get a quick
+							response. This is because questions with higher tip amounts are
+							given to doctors first.
 						</Body>
 						<MainButton onPress={() => setModalVisible(false)} text={'OK'} />
 					</Container>
